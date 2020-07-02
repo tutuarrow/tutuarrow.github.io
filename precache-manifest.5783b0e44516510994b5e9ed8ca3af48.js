6cad716b625c20e3158790110806855f96817012
unknown <singhayush585@gmail.com>
2020-07-03 01:36:35 +0530

    Build updated

---
+++ precache-manifest.5783b0e44516510994b5e9ed8ca3af48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5b8e902dd456b3237d30469a2ce8d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23652b8f276ecbddde3f",
+    "url": "/static/css/main.a05d086a.chunk.css"
+  },
+  {
+    "revision": "4f417f7b5f949661b00c",
+    "url": "/static/js/2.03548710.chunk.js"
+  },
+  {
+    "revision": "1cad250318cbbd3f8b3f9b81a91b3ff6",
+    "url": "/static/js/2.03548710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23652b8f276ecbddde3f",
+    "url": "/static/js/main.ae2ab243.chunk.js"
+  },
+  {
+    "revision": "953f7de11314a5b93685",
+    "url": "/static/js/runtime-main.2966df01.js"
+  },
+  {
+    "revision": "37733f88ba9549576565d55b0c63dcb0",
+    "url": "/static/media/Logo.37733f88.png"
+  }
+]);
